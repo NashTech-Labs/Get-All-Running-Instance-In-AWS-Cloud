c303d958c18d3b7fa15757f1dfd7cb9ba6710ec4
muzakkir <mohd.saifi@nashtechglobal.com>
2023-05-29 00:29:00 +0530

    update in the code

--- fetch-instance.py
+++ fetch-instance.py
@@ -1,19 +1,23 @@
 import boto3
 
 # add your credential of AWS to interact with the AWS
-aws_access_key_id = ''
-aws_secret_access_key = ''
-aws_region = ''
+aws_access_key_id = '< >'
+aws_secret_access_key = '< >'
+aws_region = '< >'
+
+# function to fetch the running Ec2 in AWS in perticular region
 
 def list_running_EC2():
+
     # boto3 client
+
     ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
     
-    # to list the running instance in your aws account.
+    # To list the running instance in your aws account.
     response = ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     
 
-    # fetch all the details of the 
+    # Fetch all the details of the running instance in aws  
     Ec2 = response['Reservations']
     for reservation in Ec2:
         for instance in reservation['Instances']:
@@ -28,8 +32,13 @@ def list_running_EC2():
             private_ip = instance.get('PrivateIpAddress', 'N/A')
             print(f"Instance ID: {Ec2_id}, Instance Name: {Ec2_name}, Instance Type: {Ec2_type}, Public IP: {public_ip}, Private IP: {private_ip}")
 
+
+# Define the Function to delete the one of running instace [ will ask you the name of running instace which you wan to delete. ]
+
 def delete_instance_by_name(Ec2_name):
+
     # Create a Boto3 client for EC2
+
     ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
 
     # Get the instance ID based on the instance name
@@ -38,18 +47,24 @@ def delete_instance_by_name(Ec2_name):
     # Extract the instance ID
     if response['Reservations']:
         instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
-        print(f"Instance found with name '{Ec2_name}', Instance ID: {instance_id}")
+        print(f"Your instance found with  this name: '{Ec2_name}', and your Instance ID is : {instance_id}")
 
         # Ask for deletion confirmation
-        delete = input(f"Delete instance '{Ec2_name}' with ID {instance_id}? (y/n): ")
+        delete = input(f"You enter this instance name '{Ec2_name}' with ID {instance_id} which you want to delete ? (y/n): ")
+
         if delete.lower() == 'y':
 
             ec2_client.terminate_instances(InstanceIds=[instance_id])
             print(f"Instance {instance_id} terminated.")
+
+
         else:
+
             print(f"Instance {instance_id} not deleted.")
     else:
-        print(f"No instance found with name '{Ec2_name}'.")
+
+        print(f"Ohh, No instance found with name '{Ec2_name}'.")
+
 
 # calling the list_running_EC2 to get the list of the Ec2 instance in your AWS cloud
 list_running_EC2()
